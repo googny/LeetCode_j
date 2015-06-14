b6246a282a41a5e16f6d313fcd522b3667c1dd67
googny <haoqit913@gmail.com>
2015-06-14 16:47:50 +0800

    CountPrimes.java 计算数值范围内的素数个数

---
+++ src/hashtable/easy/CountPrimes.java
@@ -0,0 +1,15 @@
+package hashtable.easy;
+
+/**
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * @author mti1301
+ * @since 2015/6/12.
+ */
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        // 首先，你最先想到的肯定是最慢的，一个个遍历呗，这个不是我想要的，所以就不写了
+        return 0;
+    }
+}
